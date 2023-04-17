451993a2614830ca93cd309d07fa8f8ef8163b0e
pierreaubert <pierre0aubert@gmail.com>
2023-04-17 21:39:42 +0200

    make ruff happy and fix param 5 which is a float

--- aupreset2txt.py
+++ aupreset2txt.py
@@ -4,76 +4,79 @@
 import struct
 import sys
 from base64 import b64decode
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElementTree
+
 
 def iir_type_2_string(iir_type: float) -> str:
-    iir = int(iir_type)
-    if iir == 0:
-        return "PK"
-    elif iir == 1:
-        return "BLP"
-    elif iir == 2:
-        return "BHP"
-    elif iir == 3:
-        return "RLP"
-    elif iir == 4:
-        return "RHP"
-    elif iir == 5:
-        return "BP"
-    elif iir == 6:
-        return "BS"
-    elif iir == 7:
-        return "LS"
-    elif iir == 8:
-        return "HS"
-    elif iir == 9:
-        return "RLS"
-    elif iir == 10:
-        return "RHS"
-    return "??"
+    return {
+        0: "PK",
+        1: "BLP",
+        2: "BHP",
+        3: "RLP",
+        4: "RHP",
+        5: "BP",
+        6: "BS",
+        7: "LS",
+        8: "HS",
+        9: "RLS",
+        10: "RHS",
+    }.get(int(iir_type), "??")
+
 
 def active_2_string(active: float) -> str:
     param = int(active)
     if param == 0.0:
         return "ON"
-    elif param == 1.0
+    elif param == 1.0:
         return "KO"
     return "??"
+
 
 def main():
     if len(sys.argv) != 2:
         sys.exit(-1)
     rew_filename = sys.argv[1]
-    tree = ET.parse(rew_filename)
+
+    tree = ElementTree.parse(rew_filename)
     root = tree.getroot()
     data = root.findall("./dict/data")
     ascii = data[0].text
-    lines = ''.join([a.strip() for a in ascii])
+    lines = "".join([a.strip() for a in ascii])
     bigendian = b64decode(lines)
     # debugging begining of buffer
-    # for i in range(0, 5):
-    #    debug = struct.unpack('>L', bigendian[i*4:i*4+4])
-    #    # print("{:1d} {}".format(i, debug))
-    # 
-    count = 80
+    for i in range(0, 5):
+        debug_i = struct.unpack(">L", bigendian[i * 4 : i * 4 + 4])
+        debug_f = struct.unpack(">f", bigendian[i * 4 : i * 4 + 4])
+        print("{:1d} {} {}".format(i, debug_i, debug_f))
+
+    i = 2
+    count = struct.unpack(">L", bigendian[i * 4 : i * 4 + 4])[0] - 1
+    i = 4
+    db_gain = struct.unpack(">f", bigendian[i * 4 : i * 4 + 4])[0]
+
+    print("count={} db_gain={}".format(count, db_gain))
+
     preset = {}
     for i in range(0, count):
-        offset = 20+i*8
-        param_id = struct.unpack('>L', bigendian[offset:offset+4])
-        value = struct.unpack('>f', bigendian[offset+4:offset+8])
-        preset["{}".format(param_id[0])] = value[0]
-        # print('param_id {} value {}'.format(param_id, value))
+        offset = 20 + i * 8
+        param_id = struct.unpack(">L", bigendian[offset : offset + 4])
+        value_f = struct.unpack(">f", bigendian[offset + 4 : offset + 8])
+        print("param_id {} value {}".format(param_id, value_f))
+        preset["{}".format(param_id[0])] = value_f[0]
 
-    iir = {}
     for i in range(0, 15):
-        iir_type = preset["{}".format(1000+i)]
-        active = preset["{}".format(2000+i)]
-        freq = preset["{}".format(3000+i)]
-        gain = preset["{}".format(4000+i)]
-        width = preset["{}".format(5000+i)]
-        print("{:2s} {:2s} {:5.0f}Hz {:+2.2f}dB {:1.2f}".format(
-            active_2_string(active), iir_type_2_string(iir_type), freq, gain, width))
+        active = preset["{}".format(1000 + i)]
+        iir_type = preset["{}".format(2000 + i)]
+        freq = preset["{}".format(3000 + i)]
+        gain = preset["{}".format(4000 + i)]
+        width = preset["{}".format(5000 + i)]
+        print(
+            "{:2s} {:3s} {:5.0f}Hz {:+2.2f}dB {:1.2f}".format(
+                active_2_string(active), iir_type_2_string(iir_type), freq, gain, width
+            )
+        )
     return 0
+
 
 if __name__ == "__main__":
     STATUS = main()
--- rew2aupreset.py
+++ rew2aupreset.py
@@ -6,7 +6,8 @@ import sys
 import base64
 from string import Template
 
-aupreset_template = Template('\
+aupreset_template = Template(
+    '\
 <?xml version="1.0" encoding="UTF-8"?>\n\
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n\
 <plist version="1.0">\n\
@@ -31,104 +32,106 @@ $data\n\
 	<integer>0</integer>\n\
 </dict>\n\
 </plist>\n\
-')
+'
+)
 
-def rew2iir(filename: str) -> list[dict]:
-    #has_width = False
+# type of IIR
+IIR = list[dict[str, int | float]]
+
+
+def rew2iir(filename: str) -> tuple[int, IIR]:
+    # has_width = False
     iir = []
-    with open(filename, "r", encoding='ASCII') as fd:
+    with open(filename, "r", encoding="ASCII") as fd:
         lines = fd.readlines()
         for line in lines:
-            #if line.find("AU_N-Band_EQ"):
+            # if line.find("AU_N-Band_EQ"):
             #    has_width = True
             tokens = line.split()
             if len(tokens) != 8 or tokens[0] == "Number" or tokens[3] == "None":
                 continue
-            iir.append({
-                "type": tokens[3],
-                "freq": float(tokens[4]),
-                "gain": float(tokens[5]),
-                "width": float(tokens[6]),
-            })
+            iir.append(
+                {
+                    "type": tokens[3],
+                    "freq": float(tokens[4]),
+                    "gain": float(tokens[5]),
+                    "width": float(tokens[6]),
+                }
+            )
 
-    return iir
+    return 0, iir
 
 
-def iir2data(iir: list) -> str:
+def iir2data(iir: IIR) -> str:
+    """Build the data field from an iir"""
 
-    def type2value(t):
-        if t == "PK":
-            return 0.0
-        elif t == "LS":
-            return 8.0
-        elif t == "HS":
-            return 9.0
-        elif t == "BP":
-            return 6.0
-        return -1.0
-       
+    def type2value(t: str) -> float:
+        """Transform a IIR type into the corresponding value for AUNBandEQ"""
+        return {
+            "PK": 0.0,
+            "LS": 8.0,
+            "HS": 9.0,
+            "BP": 6.0,
+        }.get(t, -1.0)
+
     len_iir = len(iir)
-    #print(len_iir)
-    #print(iir)
+    # print(len_iir)
+    # print(iir)
 
     params = {}
     for i, current_iir in enumerate(iir):
-        params["{}".format(1000+i)] = 0.0
-        params["{}".format(2000+i)] = type2value(current_iir["type"])
-        params["{}".format(3000+i)] = float(current_iir["freq"])
-        params["{}".format(4000+i)] = float(current_iir["gain"])
-        params["{}".format(5000+i)] = float(current_iir["width"])
+        params["{}".format(1000 + i)] = 0.0  # True
+        params["{}".format(2000 + i)] = type2value(current_iir["type"])
+        params["{}".format(3000 + i)] = float(current_iir["freq"])
+        params["{}".format(4000 + i)] = float(current_iir["gain"])
+        params["{}".format(5000 + i)] = float(current_iir["width"])
 
+    # remainings EQ are required and are set to 0
     for i in range(len_iir, 16):
-        params["{}".format(1000+i)] = 1.0
-        params["{}".format(2000+i)] = 0.0
-        params["{}".format(3000+i)] = 0.0
-        params["{}".format(4000+i)] = 0.0
-        params["{}".format(5000+i)] = 0.0
+        params["{}".format(1000 + i)] = 1.0  # False
+        params["{}".format(2000 + i)] = 0.0
+        params["{}".format(3000 + i)] = 0.0
+        params["{}".format(4000 + i)] = 0.0
+        params["{}".format(5000 + i)] = 0.0
 
-    #    print(len(params))
-    #    print(params)
-    assert len(params) == 80
-
-    # some black magic, data is padded, the only important value is number of parameters + 1
-    buffer = struct.pack(">lllll", 0, 0, 81, 0, 0)
+    # some black magic, data is padded, the only important values are
+    # 3. number of parameters + 1
+    # 5. db_gain
+    buffer = struct.pack(">llllf", 0, 0, 81, 0, 0.0)
 
     # add pairs of (param_id, value) in big endian
     for param_id, value in params.items():
         buffer += struct.pack(">lf", int(param_id), value)
 
     # convert the byte buffer to base64
-    text = base64.standard_b64encode(buffer).decode('ascii')
+    text = base64.standard_b64encode(buffer).decode("ascii")
 
     # add \t and slice in chunks of 67 chars
-    len_text = len(text)//67
-    slices = ["\t{}\n".format(text[i*67:(i+1)*67]) for i in range(len_text)]
-    slices += ["\t{}".format(text[len_text*67:])]
+    len_text = len(text) // 67
+    slices = ["\t{}\n".format(text[i * 67 : (i + 1) * 67]) for i in range(len_text)]
+    slices += ["\t{}".format(text[len_text * 67 :])]
     return "".join(slices)
 
 
-def iir2aupreset(iir: list, name: str) -> str:
-    number_of_bands = len(iir)
-    data = iir2data(iir)
-    return aupreset_template.substitute(
-        data=data,
-        name=name,
-        number_of_bands=16
-    )
-                             
+def iir2aupreset(iir: list, name: str) -> tuple[int | str]:
+    status, data = iir2data(iir)
+    status, templ = aupreset_template.substitute(data=data, name=name, number_of_bands=16)
+    return status, templ
+
 
 def main():
     if len(sys.argv) != 2:
         sys.exit(-1)
     rew_filename = sys.argv[1]
 
-    iir = rew2iir(rew_filename)
-    if len(iir) == 0:
+    status, iir = rew2iir(rew_filename)
+    if status != 0 or len(iir) == 0:
         return 1
 
-    aupreset = iir2aupreset(iir, 'test')
+    status, aupreset = iir2aupreset(iir, "test")
     print(aupreset)
-    return 0
+    return status
+
 
 if __name__ == "__main__":
     sys.exit(main())

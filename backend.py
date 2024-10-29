49c3b0c744cd727b961ab51971d6a2ef2fb447a8
Pierre Aubert <1709343+pierreaubert@users.noreply.github.com>
2024-10-29 18:06:06 +0100

    phase 1 and 2 working

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 import sqlite3
-from fastapi import FastAPI, Depends, File, UploadFile
+from fastapi import FastAPI, Depends, File, UploadFile, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
@@ -15,8 +15,8 @@ import uvicorn
 from pydantic import BaseModel, Field
 import numpy as np
 
-from iir.filter_iir import Peq
-from iir.filter_peq import peq_format_apo, peq_print, peq_build
+from iir.filter_iir import Biquad
+from iir.filter_peq import peq_format_apo, peq_build
 from converter import IIR, lines2iir, iir2aupreset, iir2peq
 
 
@@ -39,8 +39,10 @@ SPIN = "/var/www/html/spinorama-prod"
 METADATA = f"{SPIN}/assets/metadata.json"
 EQDATA = f"{SPIN}/assets/eqdata.json"
 FASTAPI_DEBUG = False
+SERVER = "https://eqconvert.spinorama.org/{}".format(API_VERSION)
 
 if ENV == "dev":
+    SERVER = "http://0.0.0.0:8000/{}".format(API_VERSION)
     SPIN = "/Users/pierrre/src/spinorama/docs/json"
     METADATA = f"{SPIN}/metadata.json"
     EQDATA = f"{SPIN}/eqdata.json"
@@ -71,8 +73,7 @@ def create_table():
     cursor.execute(
         """
         CREATE TABLE IF NOT EXISTS eqs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        hash TEXT NOT NULL,
+        hash TEXT PRIMARY KEY,
         name TEXT NOT NULL,
         peq  TEXT NOT NULL
         )
@@ -86,7 +87,7 @@ def create_eq(eq: EQ) -> int:
     connection = create_connection()
     cursor = connection.cursor()
     cursor.execute(
-        "INSERT INTO eqs (hash, name, peq) VALUES (?, ?, ?)",
+        "INSERT INTO eqs (hash, name, peq) VALUES (?, ?, ?) ON CONFLICT (hash) DO UPDATE SET name=excluded.name",
         (eq.hash, eq.name, eq.peq),
     )
     connection.commit()
@@ -198,13 +199,82 @@ async def get_speaker_eqdata(
     eqdata: dict = Depends(load_eqdata),  # noqa: B008
 ):
     content = eqdata.get(speaker_name, {"error": "Speaker not found"})
-    encoded = jsonable_encoder(content)
+    flat = []
+    if "eqs" in content.keys():
+        for key in content["eqs"]:
+            eq = content["eqs"][key]
+            lines = []
+            lines.append(
+                "{} {}".format(
+                    eq["display_name"],
+                    eq["filename"],
+                )
+            )
+            lines.append("\n")
+            lines.append("Preamp gain: {:+3.1f}".format(eq["preamp_gain"]))
+            for i, iir in enumerate(eq["peq"]):
+                iir_type = Biquad.type2name[iir["type"]][1]
+                iir_freq = int(iir["freq"])
+                iir_Q = iir["Q"]
+                iir_dbGain = iir["dbGain"]
+                lines.append(
+                    "Filter {:2d} ON {:s} Fc {:d} Hz Gain {:4.1f} dB Q {:4.2f}".format(
+                        i,
+                        iir_type,
+                        iir_freq,
+                        iir_dbGain,
+                        iir_Q,
+                    )
+                )
+            lines.append("\n")
+            buffer = "\n".join(lines).encode(encoding="utf-8")
+            status, hash_or_msg = storeEQ(speaker_name, buffer)
+            if not status:
+                raise HTTPException(status_code=500, detail=hash_or_msg)
+            eq["hash"] = hash_or_msg
+            flat.append(
+                {
+                    "hash": hash_or_msg,
+                    "eq": "\n".join(lines),
+                    "display_name": eq["display_name"],
+                    "name": key,
+                }
+            )
+    encoded = jsonable_encoder(flat)
     return JSONResponse(content=encoded)
+
+
+def eq2hash(buffer: bytes) -> str:
+    return hashlib.blake2b(buffer).hexdigest()
+
+
+def storeEQ(filename: str, buffer: bytes) -> tuple[bool, str]:
+    input = buffer.decode("utf-8")
+    if not input or len(input) == 0:
+        return False, "There was an error parsing the file: buffer decoding"
+    lines = input.split("\n")
+    if not lines or len(lines) == 0:
+        return False, "There was an error parsing the file: buffer splitting"
+    status, iir = lines2iir(lines)
+    if status != 0:
+        return False, "There was an error parsing the file as an EQ"
+    hash = eq2hash(buffer)
+    if not hash:
+        return (
+            False,
+            "There was an error computing the hash failed",
+        )
+    name = filename if filename else "eq"
+    eq = EQ(hash=hash, name=name, peq=str(iir))
+    status = create_eq(eq)
+    if status != 0:
+        return False, "Failed to save peq"
+    return True, hash
 
 
 @backend.post(f"/{API_VERSION}/eq/upload", tags=["EQ"])
 async def upload_eq(file: UploadFile = File(...)):
-    hash: str = ""
+    hash_or_msg: str = ""
     try:
         buffer = await file.read()
         if not buffer or len(buffer) == 0:
@@ -212,44 +282,27 @@ async def upload_eq(file: UploadFile = File(...)):
                 "message": "There was an error parsing the file: buffer looks empty",
                 "hash": None,
             }
-        input = buffer.decode("utf-8")
-        if not input or len(input) == 0:
+        if file.filename is None:
             return {
-                "message": "There was an error parsing the file: buffer decoding",
+                "message": "There was an error with the name of the file",
                 "hash": None,
             }
-        lines = input.split("\n")
-        if not lines or len(lines) == 0:
+        status, hash_or_msg = storeEQ(file.filename, buffer)
+        if not status:
             return {
-                "message": "There was an error parsing the file: buffer splitting",
+                "message": hash_or_msg,
                 "hash": None,
             }
-        status, iir = lines2iir(lines)
-        if status != 0:
-            return {
-                "message": "There was an error parsing the file as an EQ",
-                "hash": None,
-            }
-        hash = hashlib.blake2b(buffer).hexdigest()
-        if not hash:
-            return {
-                "message": "There was an error parsing the file: hashing failed",
-                "hash": None,
-            }
-        name = file.filename if file.filename else "eq"
-        eq = EQ(hash=hash, name=name, peq=str(iir))
-        status = create_eq(eq)
-        if status != 0:
-            return {"message": "Failed to save peq", "hash": None}
     except Exception as e:
         return {
             "message": "There was an error uploading the file {}".format(e),
             "hash": None,
         }
-
     await file.close()
-
-    return {"message": f"Successfuly uploaded {file.filename}", "hash": hash}
+    return {
+        "message": f"Successfuly uploaded {file.filename}",
+        "hash": hash_or_msg,
+    }
 
 
 @backend.get(f"/{API_VERSION}/eqs", tags=["EQ"])
@@ -280,9 +333,9 @@ async def get_eq_apo(hash: str):
 async def get_eq_graph_spl(hash: str):
     name, iir = db_get_eq(hash)
     peq = iir2peq(iir)
-    freq = np.logspace(1+math.log10(2), 4+math.log10(2), 200)
+    freq = np.logspace(1 + math.log10(2), 4 + math.log10(2), 200)
     spl = peq_build(freq, peq)
-    content = {'freq': freq.tolist(), 'spl': spl.tolist()}
+    content = {"freq": freq.tolist(), "spl": spl.tolist()}
     encoded = jsonable_encoder(content)
     return JSONResponse(content=encoded)
 
@@ -291,11 +344,11 @@ async def get_eq_graph_spl(hash: str):
 async def get_eq_graph_spl_details(hash: str):
     name, iir = db_get_eq(hash)
     peq = iir2peq(iir)
-    freq = np.logspace(1+math.log10(2), 4+math.log10(2), 200)
+    freq = np.logspace(1 + math.log10(2), 4 + math.log10(2), 200)
     spl = {}
     for i, iir in enumerate(peq):
         spl[i] = peq_build(freq, [iir]).tolist()
-    content = {'freq': freq.tolist(), 'spl': spl}
+    content = {"freq": freq.tolist(), "spl": spl}
     encoded = jsonable_encoder(content)
     return JSONResponse(content=encoded)
 
--- index.js
+++ index.js
@@ -1,27 +1,67 @@
+// import Plotly from 'plotly-dist-min';
+
 const hide = (elem) => {
     if (elem) {
-	elem.hidden = true;
+        elem.hidden = true;
     }
 };
 
 const show = (elem) => {
     if (elem) {
-	elem.hidden = false;
+        elem.hidden = false;
     }
 };
 
-const https_headers = { headers: { 'Accept-Encoding': 'bz2, gzip, deflate', 'Content-Type': 'application/json' }};
+const https_headers = { headers: { 'Accept-Encoding': 'bz2, gzip, deflate', 'Content-Type': 'application/json' } };
+
+// structure of the index.html
+// select
+//    +----- a file
+//    +----- for a speaker
+//    +----- for a headset
+// adapt
+//    +----- generic peq
+//    +----- parametricEQ
+//    +----- RME
+//    +----- Roon
+// convert
+//    +----- to format
+//                  +--- APO
+//                  +--- AUPreset
+//    +----- download
+
+const stepSelect = document.querySelector('#stepSelect');
+
+const formUpload = stepSelect.querySelector('#uploadEQ');
+const formUploadInput = formUpload.querySelector('input[type=file]');
+const formUploadFilename = formUpload.querySelector('.file-name');
+
+const plots = stepSelect.querySelector('#plots');
+const plotPEQ = plots.querySelector('#plotPEQ');
+const plotIIR = plots.querySelector('#plotIIR');
+
+const selectSpeakers = document.querySelector('#selectSpeakers');
+const selectSpeakerEQ = document.querySelector('#selectSpeakerEQ');
+
+const stepConvert = document.querySelector('#stepConvert');
+const formConvert = stepConvert.querySelector('#convertEQ');
+const formConvertHash = formConvert.querySelector('#hash');
+const formConvertSelect = formConvert.querySelector('#selectFormat');
+const formConvertSubmit = formConvert.querySelector('#submitButton');
+
+const formDownloadAPO = stepConvert.querySelector('#downloadAPO');
+const formDownloadAUPreset = stepConvert.querySelector('#downloadAUPreset');
 
 async function uploadFiles(form) {
-    const url = "http://0.0.0.0:8000/v0/eq/upload";
+    const url = 'http://0.0.0.0:8000/v0/eq/upload';
     const formData = new FormData();
     for (const file of form.files) {
-	formData.append("file", file);
+        formData.append('file', file);
     }
     const fetchOptions = {
-	method: "post",
-	mode: "cors",
-	body: formData,
+        method: 'post',
+        mode: 'cors',
+        body: formData,
     };
 
     const response = await fetch(url, fetchOptions);
@@ -31,141 +71,287 @@ async function uploadFiles(form) {
     return text;
 }
 
-async function displayEqAPO(hash) {
-    const eq = document.querySelector("#displayEQAPO ul");
-    const url = "http://0.0.0.0:8000/v0/eq/target/apo";
-    if (hash === null || hash.length !== 128 ) {
-	return;
+function assignDiv(selector, dataList, defText, selected) {
+    while (selector.firstChild) {
+        selector.firstChild.remove();
     }
-    const response = await fetch(url + "?hash=" + hash, https_headers);
-
-    const data = await response.json();
-    if (eq) {
-	while (eq.firstChild) {
-	    eq.removeChild(eq.firstChild);
-	}
-	const lines = data.split("\n");
-	const fragment = new DocumentFragment();
-	lines.forEach((line) => {
-            const li = document.createElement("li");
-            li.textContent = line;
-            fragment.appendChild(li);
-	});
-	eq.appendChild(fragment);
+    const defOption = document.createElement('option');
+    defOption.value = '';
+    defOption.text = defText;
+    selector.appendChild(defOption);
+    for (const element of dataList) {
+        const currentOption = document.createElement('option');
+        currentOption.value = element;
+        currentOption.text = element.replace('Vendors-', '').replace('vendor-pattern-', 'Pattern ');
+        if (element === selected) {
+            currentOption.selected = true;
+        }
+        if (dataList.length === 1) {
+            currentOption.disabled = true;
+        }
+        selector.appendChild(currentOption);
     }
 }
 
-async function displayEqAUPreset(hash) {
-    const url = "http://0.0.0.0:8000/v0/eq/target/aupreset";
-    if (hash === null || hash.length !== 128 ) {
-	return;
-    }
-    const response = await fetch(url + "?hash=" + hash, https_headers);
+async function setSpeakerEQ(speakerName) {
+    const url = 'http://0.0.0.0:8000/v0/speaker/' + speakerName + '/eqdata';
+    const response = await fetch(url, https_headers);
 
     const data = await response.json();
-    const eq = document.querySelector("#displayEQAUpreset ul");
-    if (eq && data.length >= 1) {
-        while (eq.firstChild) {
-            eq.removeChild(eq.firstChild);
+    const labels = data.map((v) => v['name']);
+
+    if (selectSpeakerEQ) {
+        assignDiv(selectSpeakerEQ, labels, 'Select an EQ ...', '');
+        show(selectSpeakerEQ);
+    } else {
+        hide(selectSpeakerEQ);
+    }
+
+    selectSpeakerEQ.onchange = async () => {
+        const selectedSpeakerEQ = selectSpeakerEQ.value;
+        if (selectedSpeakerEQ !== '') {
+            let hash = '';
+            // must be a filter function somewhere
+            data.forEach((v) => {
+                if (v['name'] == selectedSpeakerEQ) {
+                    hash = v['hash'];
+                }
+            });
+            formConvertHash.value = hash;
+            const status1 = await plotlyPEQ(plotPEQ, hash);
+            const status2 = await plotlyIIR(plotIIR, hash);
+            if (status1 && status2) {
+                show(plots);
+            }
+            show(stepConvert);
+        } else {
+            hide(plots);
+            hide(stepConvert);
         }
-        const lines = data[1].split("\n");
-        const fragment = new DocumentFragment();
+    };
+    return true;
+}
+
+async function loadFromSpinorama() {
+    const url = 'http://0.0.0.0:8000/v0/speakers';
+    const response = await fetch(url, https_headers);
+
+    const data = await response.json();
+    if (selectSpeakers) {
+        assignDiv(selectSpeakers, data, 'Select a speaker ...', '');
+    }
+
+    selectSpeakers.onchange = async () => {
+        const selectedSpeaker = selectSpeakers.value;
+        if (selectedSpeaker !== '') {
+            setSpeakerEQ(selectedSpeaker);
+            hide(plots);
+            hide(stepConvert);
+        }
+    };
+
+    return true;
+}
+
+async function displayEqAPO(hash) {
+    const eq = document.querySelector('#displayEQAPO p');
+    const url = 'http://0.0.0.0:8000/v0/eq/target/apo';
+    if (hash === null || hash.length !== 128) {
+        return;
+    }
+    const response = await fetch(url + '?hash=' + hash, https_headers);
+
+    const data = await response.json();
+    if (eq) {
+        const lines = data.split('\n');
+        let content = '';
         lines.forEach((line) => {
-            const li = document.createElement("li");
-            li.textContent = line;
-            fragment.appendChild(li);
+            content += line + '<br/>';
         });
-        eq.appendChild(fragment);
+        eq.innerHTML = content;
+    }
+}
+
+async function downloadAPO(fileName, hash) {
+    const url = 'http://0.0.0.0:8000/v0/eq/target/apo';
+    if (hash === null || hash.length !== 128) {
+        return;
+    }
+    const response = await fetch(url + '?hash=' + hash, https_headers);
+
+    const data = await response.json();
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8, ' + encodeURIComponent(data));
+    element.setAttribute('download', fileName);
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+async function downloadAUPreset(fileName, hash) {
+    const url = 'http://0.0.0.0:8000/v0/eq/target/aupreset';
+    if (hash === null || hash.length !== 128) {
+        return;
+    }
+    const response = await fetch(url + '?hash=' + hash, https_headers);
+    const data = await response.json();
+
+    let aupresetName = fileName;
+    if (aupresetName.length > 4) {
+        aupresetName = fileName.slice(0, -4) + '.aupreset';
+    }
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8, ' + encodeURIComponent(data));
+    element.setAttribute('download', aupresetName);
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+function xml2html(s) {
+    return s.replace(/[<>"'&]/g, function (char) {
+        switch (char) {
+            case '<':
+                return '&lt;';
+            case '>':
+                return '&gt;';
+            case '"':
+                return '&quot;';
+            case "'":
+                return '&apos;';
+            case '&':
+                return '&amp;';
+        }
+    });
+}
+
+async function displayEqAUPreset(hash) {
+    const url = 'http://0.0.0.0:8000/v0/eq/target/aupreset';
+    if (hash === null || hash.length !== 128) {
+        return;
+    }
+    const response = await fetch(url + '?hash=' + hash, https_headers);
+
+    const data = await response.json();
+    const eq = document.querySelector('#displayEQAUpreset p');
+    if (eq && data.length >= 1) {
+        const lines = data[1].split('\n');
+        let content = '';
+        lines.forEach((line) => {
+            content += xml2html(line) + '<br/>';
+        });
+        eq.innerHTML = content;
     }
 }
 
 function peq2graph(freq, spl) {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const data = [{
-	x: freq,
-	y: spl,
-	type: 'scatter'
-    }]
+    const ww = Math.min(window.innerWidth, 600);
+    const wh = window.innerHeight;
+    let w = ww - 40;
+    let h = ww / 2;
+    if (ww < wh) {
+        let h = wh - 40;
+        let w = wh / 2;
+    }
+    const data = [
+        {
+            x: freq,
+            y: spl,
+            type: 'scatter',
+        },
+    ];
     const layout = {
-	width: w-40,
-	height: w/2,
-	xaxis: {
-	    title: {
-		text: 'Freq (Hz)'
-	    },
-	    type: "log",
-	    range: [Math.log10(20), Math.log10(20000)],
-	    showline: true,
-	    dtick:"D1"
-	},
-	yaxis: {
-	    title: {
-		text: 'SPL (dB)'
-	    }
-	},
-	margin: {
-	    l: 40,
-	    r: 120,
-	    t: 10,
-	    b: 60
-	}
-    }
+        width: w,
+        height: h,
+        xaxis: {
+            title: {
+                text: 'Freq (Hz)',
+            },
+            type: 'log',
+            range: [Math.log10(20), Math.log10(20000)],
+            showline: true,
+            dtick: 'D1',
+        },
+        yaxis: {
+            title: {
+                text: 'SPL (dB)',
+            },
+        },
+        margin: {
+            l: 40,
+            r: 120,
+            t: 10,
+            b: 60,
+        },
+    };
     const config = {
-	responsive: false,
-	displayModeBar: false
-    }
+        responsive: false,
+        displayModeBar: false,
+    };
     return [data, layout, config];
 }
 
 function iir2graph(freq, spls) {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const data = []
-    spls.forEach( (value) => {
-	data.push({
-	    x: freqs,
-	    y: value,
-	    type: 'scatter'
-	});
-    });
+    const ww = Math.min(window.innerWidth, 600);
+    const wh = window.innerHeight;
+    let w = ww - 40;
+    let h = ww / 2;
+    if (ww < wh) {
+        let h = wh - 40;
+        let w = wh / 2;
+    }
+
+    const data = [];
+    // spls is a js object without any method ....
+    for (let i = 0; i < 32; i++) {
+        const value = spls[i];
+        if (value === undefined) {
+            break;
+        }
+        data.push({
+            x: freq,
+            y: value,
+            showlegend: false,
+            type: 'scatter',
+        });
+    }
     const layout = {
-	width: w-40,
-	height: w/2,
-	xaxis: {
-	    title: {
-		text: 'Freq (Hz)'
-	    },
-	    type: "log",
-	    range: [Math.log10(20), Math.log10(20000)],
-	    showline: true,
-	    dtick:"D1"
-	},
-	yaxis: {
-	    title: {
-		text: 'SPL (dB)'
-	    }
-	},
-	margin: {
-	    l: 40,
-	    r: 120,
-	    t: 10,
-	    b: 60
-	}
-    }
+        width: w,
+        height: h,
+        xaxis: {
+            title: {
+                text: 'Freq (Hz)',
+            },
+            type: 'log',
+            range: [Math.log10(20), Math.log10(20000)],
+            showline: true,
+            dtick: 'D1',
+        },
+        yaxis: {
+            title: {
+                text: 'SPL (dB)',
+            },
+        },
+        margin: {
+            l: 40,
+            r: 120,
+            t: 10,
+            b: 60,
+        },
+    };
     const config = {
-	responsive: false,
-	displayModeBar: false
-    }
+        responsive: false,
+        displayModeBar: false,
+    };
     return [data, layout, config];
 }
 
 async function plotlyPEQ(div, hash) {
-    const url = "http://0.0.0.0:8000/v0/eq/graph_spl";
-    if (hash === null || hash.length !== 128 ) {
-	return;
+    const url = 'http://0.0.0.0:8000/v0/eq/graph_spl';
+    if (hash === null || hash.length !== 128) {
+        return;
     }
-    const response = await fetch(url + "?hash=" + hash, https_headers);
+    const response = await fetch(url + '?hash=' + hash, https_headers);
     const data = await response.json();
     const specs = peq2graph(data.freq, data.spl);
     Plotly.newPlot(div, specs[0], specs[1], specs[2]);
@@ -173,11 +359,11 @@ async function plotlyPEQ(div, hash) {
 }
 
 async function plotlyIIR(div, hash) {
-    const url = "http://0.0.0.0:8000/v0/eq/graph_spl_iir";
-    if (hash === null || hash.length !== 128 ) {
-	return;
+    const url = 'http://0.0.0.0:8000/v0/eq/graph_spl_details';
+    if (hash === null || hash.length !== 128) {
+        return;
     }
-    const response = await fetch(url + "?hash=" + hash, https_headers);
+    const response = await fetch(url + '?hash=' + hash, https_headers);
     const data = await response.json();
     const specs = iir2graph(data.freq, data.spl);
     Plotly.newPlot(div, specs[0], specs[1], specs[2]);
@@ -185,55 +371,61 @@ async function plotlyIIR(div, hash) {
 }
 
 window.onload = () => {
-
     if (window.trustedTypes && window.trustedTypes.createPolicy && !window.trustedTypes.defaultPolicy) {
-	window.trustedTypes.createPolicy('default', {
-            createHTML: string => string
-	});
+        window.trustedTypes.createPolicy('default', {
+            createHTML: (string) => string,
+        });
     }
 
-    const stepSelect = document.querySelector('#stepSelect');
-    const stepConvert = document.querySelector('#stepConvert');
+    // load initial data
+    const statusLoadSpinorama = loadFromSpinorama();
+    if (!statusLoadSpinorama) {
+        console.log('Load speaker database failed!');
+    }
 
-    const formUpload = stepSelect.querySelector("#uploadEQ");
-    const formUploadInput = formUpload.querySelector("input[type=file]");
-    const formUploadFilename = formUpload.querySelector(".file-name");
-    const plots = stepSelect.querySelector("#plots");
-    const plotPEQ = plots.querySelector("#plotPEQ");
-    const plotIIR = plots.querySelector("#plotIIR");
-
-    const formConvert = stepConvert.querySelector("#convertEQ");
-    const formConvertHash = formConvert.querySelector("#hash");
-    const formConvertSelect = formConvert.querySelector("#selectFormat");
-    const formConvertSubmit = formConvert.querySelector("#submitButton");
-    const results = stepConvert.querySelector("#displayEQ");
-
+    // Upload
     formUploadInput.onchange = async () => {
-	const response = await uploadFiles(formUploadInput);
-	if (response.hash !== null) {
-	    formConvertHash.value = response.hash;
-	    if (formUploadInput.files.length > 0) {
-		formUploadFilename.textContent = '> ' + formUploadInput.files[0].name;
-	    }
-	    const status1 = await plotlyPEQ(plotPEQ, response.hash);
-	    const status2 = await plotlyIIR(plotIIR, response.hash);
-	    if (status1 && status2) {
-		show(plots);
-	    }
-	    show(stepConvert);
-	} else {
-	    formUploadFilename.textContent = response.message;
-	    hide(plots);
-	    hide(stepConvert);
-	}
-    }
+        const response = await uploadFiles(formUploadInput);
+        if (response.hash !== null) {
+            formConvertHash.value = response.hash;
+            if (formUploadInput.files.length > 0) {
+                formUploadFilename.textContent = '> ' + formUploadInput.files[0].name;
+            }
+            const status1 = await plotlyPEQ(plotPEQ, response.hash);
+            const status2 = await plotlyIIR(plotIIR, response.hash);
+            if (status1 && status2) {
+                show(plots);
+            }
+            show(stepConvert);
+        } else {
+            formUploadFilename.textContent = response.message;
+            hide(plots);
+            hide(stepConvert);
+        }
+    };
+
+    // Convert
+    const results = stepConvert.querySelector('#displayEQ');
 
     formConvertSubmit.onclick = async () => {
-	const hash = formConvertHash.value;
-	if (hash !== "" ) {
-	    displayEqAPO(hash);
-	    displayEqAUPreset(hash);
-	    show(results);
-	}
-    }
-}
+        const hash = formConvertHash.value;
+        if (hash !== '') {
+            displayEqAPO(hash);
+            displayEqAUPreset(hash);
+            show(results);
+        }
+    };
+
+    // Download
+    formDownloadAPO.onclick = async () => {
+        const filename = formUploadFilename.textContent.slice(2);
+        const hash = formConvertHash.value;
+        await downloadAPO(filename, hash);
+    };
+
+    formDownloadAUPreset.onclick = async () => {
+        const filename = formUploadFilename.textContent.slice(2);
+        const hash = formConvertHash.value;
+        await downloadAUPreset(filename, hash);
+    };
+};
